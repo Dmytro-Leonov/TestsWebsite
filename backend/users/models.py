aea38eb37769d0bd187c0add641c02b60e018e53
Dmytro-Leonov <leonovdmitriy02@gmail.com>
2023-03-01 13:18:16 +0200

    created custom user model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager,
+    AbstractBaseUser
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, full_name, email, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        if not full_name:
+            raise ValueError('Users must have a full name')
+
+        user = self.model(
+            full_name=full_name,
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, full_name, email, password=None):
+        user = self.create_user(
+            full_name,
+            email,
+            password=password
+        )
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    full_name = models.CharField(max_length=50)
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+    REQUIRED_FIELDS = ['full_name']
+
+    class Meta:
+        db_table = 'user'
+
+    def __str__(self):
+        return f'{self.full_name} - {self.email}'
