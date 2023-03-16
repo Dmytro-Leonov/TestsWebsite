bf350f332fde127eb6ed128e810d9cac16c0df72
Dmytro-Leonov <leonovdmitriy02@gmail.com>
2023-03-16 20:26:12 +0200

    added models for questions

--- backend/tests_website/questions/models.py
+++ backend/tests_website/questions/models.py
@@ -1,3 +1,68 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+from common.models import BaseModel
+
+
+class QuestionPool(BaseModel):
+    user = models.ForeignKey("auth.User", on_delete=models.CASCADE, related_name="created_question_pools")
+
+    name = models.CharField(max_length=200, validators=[MinLengthValidator(1)])
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        unique_together = ("user", "name")
+
+
+class Question(BaseModel):
+    original_question = models.ForeignKey(
+        "self",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="copies_in_tests"
+    )
+    question_pool = models.ForeignKey("QuestionPool", on_delete=models.CASCADE, related_name="questions")
+
+    question = models.TextField(max_length=2000, validators=[MinLengthValidator(1)])
+    is_original = models.BooleanField(default=True)
+
+    # order in question pool calculated using lexorank algorithm
+    order = models.CharField()
+
+    def __str__(self):
+        return self.question
+
+    class Meta:
+        unique_together = [
+            ("question_pool", "question"),
+            ("question_pool", "order")
+        ]
+
+
+class Answer(BaseModel):
+    question = models.ForeignKey("Question", on_delete=models.CASCADE, related_name="answers")
+    original_answer = models.ForeignKey(
+        "self",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="copies_in_tests"
+    )
+
+    answer = models.TextField(max_length=2000, validators=[MinLengthValidator(1)])
+    is_correct = models.BooleanField()
+
+    # order in question pool calculated using lexorank algorithm
+    order = models.CharField()
+
+    def __str__(self):
+        return self.answer
+
+    class Meta:
+        unique_together = [
+            ("question", "answer"),
+            ("question", "order")
+        ]
